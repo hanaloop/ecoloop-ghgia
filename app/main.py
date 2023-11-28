2f3f1446b535f4096be0537234426fe5d416ebe6
dionysiost <dionysios.t@hanaloop.com>
2023-11-28 11:07:06 +0900

    added dependencies, initial api endpoint

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from typing import List, Dict, Optional
+
+from fastapi import FastAPI
+import uvicorn
+
+from pydantic import BaseModel
+
+
+
+app = FastAPI()
+
+
+
+@app.get("/api/info")
+async def info():
+    # Method can be either "armia" or "auto_arima"
+    return "Hello World"
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9090)
+
+"""
+run for development (auto-reload)
+    uvicorn main:app --reload --port 9090
+"""
