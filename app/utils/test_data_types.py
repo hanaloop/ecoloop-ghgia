16ae6ef830986fe57a5bcb537c71317486401ada
dionysiost <dionysios.t@hanaloop.com>
2024-01-04 17:43:47 +0900

    fixed failing test

--- app/utils/test_data_types.py
+++ app/utils/test_data_types.py
@@ -75,9 +75,7 @@ def test_parse_to_date():
     assert parse_to_date("01/01/2021", dt_boundary_from=datetime( 2020, 1, 1), dt_boundary_to=datetime( 2021, 1, 1)) == datetime(2021, 1, 1)
     
     # Testing when value is not a valid
-    with pytest.raises(ValueError):
-        parse_to_date("2021-13-01", dt_boundary_from=datetime( 2020, 1, 1), dt_boundary_to=datetime( 2021, 1, 1))
-
+    assert parse_to_date("2021-13-01", dt_boundary_from=datetime( 2020, 1, 1), dt_boundary_to=datetime( 2021, 1, 1)) == None
     # Testing empty string
     assert parse_to_date("", dt_boundary_from=datetime( 2020, 1, 1), dt_boundary_to=datetime( 2021, 1, 1)) == None
 
