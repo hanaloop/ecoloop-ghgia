e51e0d9ce66ce24798381ab8174bcdde7722af22
dionysiost <dionysios.t@hanaloop.com>
2024-01-05 11:11:09 +0900

    removed default  path value from code script

--- scripts/script_import_code.py
+++ scripts/script_import_code.py
@@ -3,8 +3,8 @@ from app.code.service import CodeService
 from app.database import get_connection
 from app.foundation.arg_parse import parse_args
 
-# @parse_args
-async def main(path: str=".seed_data/code.ecoloop.import.csv"):
+@parse_args
+async def main(path: str ):
     db = get_connection()
     await db.connect()
     service = CodeService()
