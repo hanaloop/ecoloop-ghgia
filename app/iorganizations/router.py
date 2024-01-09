cbb73d52ca57822a71cf2037299d78e295992cef
dionysiost <dionysios.t@hanaloop.com>
2024-01-09 13:02:54 +0900

    changed org link method to use request object

--- app/iorganizations/router.py
+++ app/iorganizations/router.py
@@ -86,6 +86,10 @@ async def upload(file: UploadFile):
     data_source = file.filename
     return await service.upload_organizations(data_source=data_source, buffer=file.file)
 
-@router.get("/iorganizations-find-links/{companyName}/")
-async def find_links(companyName: str):
+@router.get("/iorganizations-find-links/")
+async def find_links(request: Request):
+    companyName = request.query_params.get("companyName")
+    if not companyName:
+        raise HTTPException(status_code=400, detail="Bad request")
+    companyName = companyName.strip()
     return await service.find_related_sites(company_name=companyName)
